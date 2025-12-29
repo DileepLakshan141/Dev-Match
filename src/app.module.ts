8616589d23387505fd392cd2cd65d481e924ca38
DileepLakshan141 <dileeplakshan212@gmail.com>
2025-12-29 12:40:41 +0530

    middleware implemention and setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('songs') - option 1
+    // option 2 is passing object with specific path
+    // option 3 is passing the controller
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'songs', method: RequestMethod.POST });
+  }
+}
